73ee4581fc94141ab1010c3d95a04133e7424fbf
Nguyễn Thành Long <longthanhnguyen0108@gmail.com>
2022-08-01 10:02:08 +0700

    updated at 2022/08/01 10:00:00

--- controller/Controller.php
+++ controller/Controller.php
@@ -2,11 +2,16 @@
 
 class Controller
 {
+    const DB_SERVER   = 'localhost:3306';
+    const DB_USERNAME = 'root';
+    const DB_PASSWORD = '';
+    const DB_DATABASE = 'sun_login';
+
     protected $database;
 
     public function __construct()
     {
-        $this->database = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);;
+        $this->database = mysqli_connect(self::DB_SERVER, self::DB_USERNAME, self::DB_PASSWORD, self::DB_DATABASE);;
     }
 
     /**
